5328ead967968b07393b2766693b63d8574e4605
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-04-11 20:58:59 +0500

    Add site-header. Update dark-mode.

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const elDarkModeToggle = document.querySelector('.site-dark-mode-toggle');
+elDarkModeToggle.addEventListener('click', function () {
+document.body.classList.toggle('dark-mode');
+});
